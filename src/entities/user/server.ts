43ee15d529af82e14558897b518735b28bbb4b6d
Kiriil <kirillprofi007@gmail.com>
2025-01-03 19:26:52 +0300

    ft: added half footer

--- src/entities/user/server.ts
+++ src/entities/user/server.ts
@@ -1,1 +1,7 @@
 export type { UserEntity } from "./domain";
+export { createUser } from "./services/create-user";
+export { getUser } from "./repositories/get-user";
+export { saveUser } from "./repositories/save-user";
+export { verifyUserPassword } from "./services/verify-user-password";
+export { getCurrentUser } from "./services/get-current-user";
+export { sessionService } from "./services/session";
--- src/widgets/footer/ui/user-menu.tsx
+++ src/widgets/footer/ui/user-menu.tsx
@@ -1,19 +1,35 @@
+import { sessionService } from "@/entities/user/server";
 import {
     DropdownMenu,
     DropdownMenuTrigger,
     DropdownMenuContent,
     DropdownMenuItem,
 } from "@radix-ui/react-dropdown-menu";
+import { redirect } from "next/navigation";
 import { FC } from "react";
 
 export const UserMenu: FC = async () => {
-    return (
-        <DropdownMenu>
-            <DropdownMenuTrigger>Открыть</DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuItem>Профиль</DropdownMenuItem>
-                <DropdownMenuItem>Выйти</DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+    const { isAuth } = await sessionService.verifySession();
+
+    return isAuth === true ? (
+        <>
+            <div>{isAuth}</div>{" "}
+            <DropdownMenu>
+                <DropdownMenuTrigger>Открыть</DropdownMenuTrigger>
+                <DropdownMenuContent>
+                    <DropdownMenuItem>Профиль</DropdownMenuItem>
+                    <DropdownMenuItem>Выйти</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </>
+    ) : (
+        <form
+            action={async () => {
+                "use server";
+                redirect("/sign-in");
+            }}
+        >
+            <DropdownMenuTrigger>Авторизироваться</DropdownMenuTrigger>
+        </form>
     );
 };
